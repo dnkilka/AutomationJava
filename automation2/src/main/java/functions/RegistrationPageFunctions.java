bc2a4c8aab807496724154cd1a693059bb0a6283
Iryna Shcherbyna <Iryna.Shcherbyna@safelite.com>
2017-12-23 09:10:23 -0600

    Improved formatting of the code

--- automation2/src/main/java/functions/RegistrationPageFunctions.java
+++ automation2/src/main/java/functions/RegistrationPageFunctions.java
@@ -11,25 +11,30 @@ import utils.Customer;
 
 public class RegistrationPageFunctions {
 
-	public static void populatePassword(WebDriver driver, Customer customer){
+	public static void populatePassword(WebDriver driver, Customer customer)
+	{
 		driver.findElement(RegistrationPage.passwordInput).sendKeys(customer.password);
 		driver.findElement(RegistrationPage.confirmPassword).sendKeys(customer.password);
 	}
 
-	public static void populateNames(WebDriver driver, Customer customer){
-	driver.findElement(RegistrationPage.firstNameInput).sendKeys(customer.firstName);
-	driver.findElement(RegistrationPage.lastNameInput).sendKeys(customer.lastName);
-	    }
+	public static void populateNames(WebDriver driver, Customer customer)
+	{
+		driver.findElement(RegistrationPage.firstNameInput).sendKeys(customer.firstName);
+		driver.findElement(RegistrationPage.lastNameInput).sendKeys(customer.lastName);
+	}
 
-	public static void populateEmail(WebDriver driver, Customer customer) {
+	public static void populateEmail(WebDriver driver, Customer customer) 
+	{
 	    driver.findElement(RegistrationPage.emailInput).sendKeys(customer.email);
 	}
 
-	public static void populateUsername(WebDriver driver, Customer customer) {
+	public static void populateUsername(WebDriver driver, Customer customer) 
+	{
 		driver.findElement(RegistrationPage.usernameInput).sendKeys(customer.username);
 	}
 	
-	public static void selectHobbyCheckbox(WebDriver driver, Customer customer) {		
+	public static void selectHobbyCheckbox(WebDriver driver, Customer customer) 
+	{		
 		List<Hobby> hobbies = Arrays.asList(customer.hobby); //convert array into list
 		
 		if(hobbies.contains(Hobby.DANCE)) {
@@ -43,7 +48,8 @@ public class RegistrationPageFunctions {
 		}
 		
 	}
-	public static void selectHobbyCheckbox1(WebDriver driver, Customer customer) {
+	public static void selectHobbyCheckbox1(WebDriver driver, Customer customer) 
+	{
 		Hobby[] hobbies = customer.hobby; 
 		for(Hobby hobby : hobbies) {
 			switch (hobby) {
